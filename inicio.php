478135495cfb6f66dd959eeeaa6046f7d70972f8
rtc <tubacrolando@gmail.com>
2021-01-22 08:47:26 -0600

    segundo commit

---
+++ inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hola mundo</title>
+
+</head>
+<body>
+    <h1>hola mundo</h1>
+
+    <h3>para ver la modificacion</h3>
+</body>
+</html>
